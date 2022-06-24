5f67af8fcc331600c9927b5e0019b90c0fca1663
godcl1623 <delphi5281@gmail.com>
2022-06-25 00:11:02 +0900

    wpof - login - login 컴포넌트 요소 배치

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -3,10 +3,21 @@ import reset from 'styled-reset';
 
 const GlobalStyles = createGlobalStyle`
   ${reset}
+  :root {
+    --color-bg: #FAFAFA;
+    --color-border: #DBDBDB;
+    --color-white: #FFFFFF;
+    --color-font: #8E8E9D;
+  }
   * {
     box-sizing:border-box;
     outline:none;
     border:none;
+  }
+  html, body, #root {
+    width: 100%;
+    height: 100%;
+    background-color: var(--color-bg);
   }
 `;
 
