41ddf2053cd7a25116f037dd1908b8db7b00168f
godcl1623 <delphi5281@gmail.com>
2022-07-04 15:37:08 +0900

    리뷰 변경3

--- src/components/Feeds/subComponents/Common/UserId.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { UserId as EmptyUserId } from '../../style/FeedsStyled';
-
-const UserId = ({ className, value }) => {
-  return <EmptyUserId className={className}>{value}</EmptyUserId>;
-};
-
-export default UserId;
